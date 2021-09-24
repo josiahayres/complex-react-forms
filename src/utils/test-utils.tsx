201beed96e178f3193999fb6c53aba8c5607e6ff
Josiah Ayres <josiahayres@gmail.com>
2021-09-25 11:43:55 +1200

    Setup tests

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../customTheme'
+
+const AllTheProviders: FC = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
